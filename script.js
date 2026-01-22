c5220e1d63c082293edbee498847a1b89671fb04
Pranav Borse <pranavborse91@gmail.com>
2026-01-22 15:59:47 +0530

    Create function to display characters

--- script.js
+++ script.js
@@ -17,3 +17,43 @@ function addCharacterToRoster(characterData) {
   const character = new Character(characterData);
   characterRoster.push(character);
 }
+
+// Loops through characterRoster array and displays each character
+function displayCharactersOnPage() {
+  main.innerHTML = "";
+  characterRoster.forEach((char) => {
+    let characterCard = document.createElement("div");
+    characterCard.classList.add("character-card");
+    characterCard.dataset.id = char.id;
+    characterCard.innerHTML = `<div class="character-name">
+        <h1>${char.name}</h1>
+        <h2 class="${char.type}">${char.type}</h2>
+    </div>
+    <div class="character-stats">
+        <div class="stat-box">
+        <h3>HP:</h3>
+        <h3 class="character-hp">${char.health}</h3>
+        </div>
+        <div class="stat-box">
+        <h3>ATK:</h3>
+        <h3 class="character-attack">${char.attack}</h3>
+        </div>
+        <div class="stat-box">
+        <h3>Mana:</h3>
+        <h3 class="character-mana">${char.mana}</h3>
+        </div>
+    </div>
+    <div class="character-level-box">
+        <div class="stat-box">
+        <h3>Level:</h3>
+        <h3 class="character-lvl">100</h3>
+        </div>
+        <div>
+        <button class="lvl-button lvl-up">+</button>
+        <button class="lvl-button lvl-down">-</button>
+        </div>
+    </div>
+    <button class="delete-button">DELETE</button>`;
+    main.appendChild(characterCard);
+  });
+}
