e668668b568c48eb5ac39224adf9ec5215d7232f
Pranav Borse <pranavborse91@gmail.com>
2026-01-22 15:59:34 +0530

    Create styles for character card

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const characterRoster = [];
+const main = document.querySelector("main");
 
+// Character constructor function
 function Character({ name, type, health, attack, mana }) {
-  ((this.id = crypto.randomUUID()),
-    (this.name = name),
-    (this.type = type),
-    (this.health = health),
-    (this.attack = attack),
-    (this.mana = mana));
+  this.id = crypto.randomUUID();
+  this.name = name;
+  this.type = type;
+  this.health = health;
+  this.attack = attack;
+  this.mana = mana;
+  this.level = 1; // prep for later
 }
 
+// Creates a new character and adds it to the characterRoster array
 function addCharacterToRoster(characterData) {
   const character = new Character(characterData);
   characterRoster.push(character);
